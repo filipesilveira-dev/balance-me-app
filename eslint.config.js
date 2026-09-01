f9bfa4c439043f48bb1eccf7d8b455398882f156
Filipe <filipesilveira.dev@gmail.com>
2026-09-01 11:28:10 -0300

    chore: commit inicial

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import someConfig from "some-other-config-you-use";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  someConfig,
+  eslintConfigPrettier,
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      "some-other-config-you-use",
+      "prettier",
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]);
